b2784a93e95127fbe625ec3d80c244b659cdabf3
Zaquariah Holland <zaquariah@gmail.com>
2021-07-09 13:54:28 -0500

    more navigation updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,26 +83,28 @@
         $(".mobile-nav, .mobile-nav-toggle").hide();
     }
 
-    // Navigation active state on scroll
-    var nav_sections = $('section');
-    var main_nav = $('.nav-menu, #mobile-nav');
-    var main_nav_height = $('#header').outerHeight();
-
-    $(window).on('scroll', function () {
-        var cur_pos = $(this).scrollTop() + 10;
-
-        nav_sections.each(function () {
-            var top = $(this).offset().top - main_nav_height,
-                bottom = top + $(this).outerHeight();
-
-            if (cur_pos >= top && cur_pos <= bottom) {
-                if (cur_pos <= bottom) {
-                    main_nav.find('li').removeClass('active');
-                }
-                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-            }
-        });
-    });
+    /*
+        Navigation active state on scroll
+     */
+    // var nav_sections = $('section');
+    // var main_nav = $('.nav-menu, #mobile-nav');
+    // var main_nav_height = $('#header').outerHeight();
+    //
+    // $(window).on('scroll', function () {
+    //     var cur_pos = $(this).scrollTop() + 10;
+    //
+    //     nav_sections.each(function () {
+    //         var top = $(this).offset().top - main_nav_height,
+    //             bottom = top + $(this).outerHeight();
+    //
+    //         if (cur_pos >= top && cur_pos <= bottom) {
+    //             if (cur_pos <= bottom) {
+    //                 main_nav.find('li').removeClass('active');
+    //             }
+    //             main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+    //         }
+    //     });
+    // });
 
     // Back to top button
     $(window).scroll(function () {
